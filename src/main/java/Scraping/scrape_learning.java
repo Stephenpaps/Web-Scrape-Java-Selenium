9a131f41dc837bc100e7b3a613d87c0e8e93d1a1
Stephen Jack <stephenjosephdurai@gmail.com>
2023-02-15 03:13:21 +0530

    Finished scraping till Screener Balance sheet

--- src/main/java/Scraping/scrape_learning.java
+++ src/main/java/Scraping/scrape_learning.java
@@ -2,7 +2,6 @@ package Scraping;
 
 import java.util.ArrayList;
 import java.util.List;
-
 import org.bson.Document;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -12,12 +11,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class scrape_learning {
@@ -26,14 +23,12 @@ public class scrape_learning {
 	public static JavascriptExecutor js;
 	MongoCollection<Document> webCollection;
 
-	@BeforeSuite
-	public void connectmongodb() {
-		MongoClient mongoClient = MongoClients.create("mongodb://127.0.0.1:27017");
-
-		MongoDatabase database = mongoClient.getDatabase("Scrape-test");
-
-		webCollection = database.getCollection("web");
-	}
+//	@BeforeSuite
+//	public void connectmongodb() {
+//		MongoClient mongoClient = MongoClients.create("mongodb://127.0.0.1:27017");
+//		MongoDatabase database = mongoClient.getDatabase("Screenercheck");
+//		webCollection = database.getCollection("Checking");
+//	}
 
 	@BeforeTest
 	public void setup() {
@@ -47,51 +42,131 @@ public class scrape_learning {
 
 	@Test
 	public void webscrapetest() {
-		driver.get("https://screener.in");
+		driver.get("https://www.screener.in/company/RELIANCE/consolidated/");
+		
+//		Document d1 = new Document();	
+		
+		// Market Cap
+		List<WebElement> marketcap = driver.findElements(By.xpath("//div[@class='company-ratios']"));
 
-		String url = driver.getCurrentUrl();
-		System.out.println("url :" + url);
-
-		String title = driver.getTitle();
-		System.out.println("title :" + title);
-
-		int linkscount = driver.findElements(By.tagName("a")).size();
-		System.out.println("linkscount :" + linkscount);
-
-		int imagecount = driver.findElements(By.tagName("img")).size();
-		System.out.println("imagecount :" + imagecount);
-
-		List<WebElement> linkslist = driver.findElements(By.tagName("a"));
-		List<String> linksAttrlist = new ArrayList();
-
-		List<WebElement> imageslist = driver.findElements(By.tagName("img"));
-		List<String> imagesrclist = new ArrayList();
-
-		Document d1 = new Document();
-		d1.append("url", url);
-		d1.append("title", title);
-		d1.append("totalLinks", linkscount);
-		d1.append("totalImages", imagecount);
-
-		for (WebElement ele : linkslist) {
-			String hrefvalue = ele.getAttribute("href");
-			linksAttrlist.add(hrefvalue);
-			System.out.println("hrefvalue :" + hrefvalue);
+		for (WebElement ele : marketcap) {
+			String marketca = ele.getText();
+			System.out.println("marketcap : " + marketca);
 		}
-
-		for (WebElement ele : imageslist) {
-			String srcvalue = ele.getAttribute("src");
-			imagesrclist.add(srcvalue);
-			System.out.println("srcvalue :" + srcvalue);
+		
+		// PeerComparison
+		WebElement PeerComparison = driver.findElement(By.xpath("//*[@id=\"peers-table-placeholder\"]/div[3]/table/tbody"));
+		WebElement PeercomparisonMedian = driver.findElement(By.xpath("//*[@id=\"peers-table-placeholder\"]/div[3]/table/tfoot"));
+		
+		List<WebElement> rows = PeerComparison.findElements(By.tagName("tr"));
+		List<WebElement> medianrow = PeercomparisonMedian.findElements(By.tagName("tr"));
+		
+		for (WebElement ele : rows) {
+			String peercomparison = ele.getText();
+			System.out.println("Peer Comparison Rows :" + peercomparison);
 		}
-
-		d1.append("linksAttribute", linksAttrlist);
-		d1.append("SrcValue", imagesrclist);
-
-		List<Document> docslist = new ArrayList();
-		docslist.add(d1);
-
-		webCollection.insertMany(docslist);
+		for (WebElement ele : medianrow) {
+			String median = ele.getText();
+			System.out.println("Peer Median : " + median);
+		}
+		
+		// Quarterly Results
+		WebElement Quarterlyresults = driver.findElement(By.xpath("//*[@id=\"quarters\"]/div[3]/table/thead"));
+		WebElement Quarterlyresults1 = driver.findElement(By.xpath("//*[@id=\"quarters\"]/div[3]/table/tbody"));
+		
+		List<WebElement> quarreslt = Quarterlyresults.findElements(By.tagName("tr"));
+		List<WebElement> quarreslt1 = Quarterlyresults1.findElements(By.tagName("tr"));
+		
+		for (WebElement ele : quarreslt) {
+			String quarterlyheading = ele.getText();
+			System.out.println("Quarterly Peers Heading :" + quarterlyheading);
+		}
+		
+		for (WebElement ele : quarreslt1) {
+			String quarterlybody = ele.getText();
+			System.out.println("Quarterly Peers Body :" + quarterlybody);
+		}
+		
+		
+		//Profit and Loss
+		WebElement Profitandlossheading = driver.findElement(By.xpath("//*[@id=\"profit-loss\"]/div[3]/table/thead"));
+		WebElement Profitandlossbody = driver.findElement(By.xpath("//*[@id=\"profit-loss\"]/div[3]/table/tbody"));
+		
+		List<WebElement> pandlhead = Profitandlossheading.findElements(By.tagName("tr"));
+		List<WebElement> pandlbody = Profitandlossbody.findElements(By.tagName("tr"));
+		
+		for (WebElement ele : pandlhead) {
+			String headpandl = ele.getText();
+			System.out.println("Heading of Profit and Loss : " + headpandl);
+		}
+		
+		for (WebElement ele : pandlbody) {
+			String pandltable = ele.getText();
+			System.out.println("Profit and Loss Table Body : " + pandltable);
+		}
+		
+		//Sales Growth
+		
+		WebElement salesgrowth = driver.findElement(By.xpath("//*[@id=\"profit-loss\"]/div[4]/table[1]/tbody"));
+		List<WebElement> compoundedsalesandgrowth = salesgrowth.findElements(By.tagName("tr"));
+		
+		for (WebElement ele : compoundedsalesandgrowth) {
+			String companysalesgrowth = ele.getText();
+			System.out.println("Compunded Sales Growth : " + companysalesgrowth);
+		}
+		
+		// Compounded Profit Growth
+		WebElement profitgrowth = driver.findElement(By.xpath("//*[@id=\"profit-loss\"]/div[4]/table[2]/tbody"));
+		List<WebElement> compoundedprofitgrowth = profitgrowth.findElements(By.tagName("tr"));
+		
+		for (WebElement ele : compoundedprofitgrowth) {
+			String companyprofitgrowth = ele.getText();
+			System.out.println("Compunded Profit Growth : " + companyprofitgrowth);
+		}
+		
+		// Stock Price CAGR
+		WebElement Stockprice = driver.findElement(By.xpath("//*[@id=\"profit-loss\"]/div[4]/table[3]/tbody"));
+		List<WebElement> StockPriceCAGR = Stockprice.findElements(By.tagName("tr"));
+		
+		for (WebElement ele : StockPriceCAGR) {
+			String SPCAGR = ele.getText();
+			System.out.println("Stock Price CAGR : " + SPCAGR);
+		}
+		
+		// Return On Equity
+		WebElement Equity = driver.findElement(By.xpath("//*[@id=\"profit-loss\"]/div[4]/table[4]/tbody"));
+		List<WebElement> retonequity = Equity.findElements(By.tagName("tr"));
+		
+		for (WebElement ele : retonequity) {
+			String returns = ele.getText();
+			System.out.println("Return on Equity : " + returns);
+		}
+		
+		
+		// Balance Sheet
+		WebElement bsheet = driver.findElement(By.xpath("//*[@id=\"balance-sheet\"]/div[2]/table/thead"));
+		WebElement bsheet1 = driver.findElement(By.xpath("//*[@id=\"balance-sheet\"]/div[2]/table/tbody"));
+		
+		List<WebElement> relbsheet = bsheet.findElements(By.tagName("tr"));
+		List<WebElement> relbsheet1 = bsheet1.findElements(By.tagName("tr"));
+		
+		for (WebElement ele : relbsheet) {
+			String relbalancesheet = ele.getText();
+			System.out.println("Balancesheet heading : " + relbalancesheet  );
+		}
+		for (WebElement ele : relbsheet1) {
+			String relbalancesheet1 = ele.getText();
+			System.out.println("Balancesheet body : " + relbalancesheet1  );
+		}
+		
+		
+		
+		
+//		d1.append("Marketcapattribute", marketcaplist);
+//
+//		List<Document> docslist = new ArrayList();
+//		docslist.add(d1);
+//		webCollection.insertMany(docslist);
 
 	}
 }
